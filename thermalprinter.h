2baaf3fe8859e073fc16944a148cbf836623588f
glender <sh@signalwerk.ch>
2012-11-29 13:58:14 -0500

    correct head and history of lib

--- thermalprinter.h
+++ thermalprinter.h
@@ -3,20 +3,23 @@
  * Name.......:  Epson TM-T88II Library
  * Description:  A Library to control the thermal printer (microprinter) Epson TM-T88II
  * Author.....:  Stefan Huber
- * Version....:  0.1
- * Date.......:  2012-03-13
+ * Version....:  0.5
+ * Date.......:  2012-04-13
  * Project....:  http://signalwerk.ch
  * Contact....:  sh[at]signalwerk[dot]ch
  * License....:  You may use this work under the terms of either the MIT License or 
                  the GNU General Public License (GPL) Version 2
- * Keywords...:  thermal, micro, receipt, printer, serial, tm-t88, tm88, tmt88, epson,
+ * Keywords...:  thermal, micro, receipt, printer, serial, tm-t88, tm88, tmt88, epson
  * Inspiration:  http://tomtaylor.co.uk/projects/microprinter
                  https://github.com/tomtaylor/microprinter-arduino/tree
                  http://rooreynolds.com/2009/02/01/microprinter/
                  https://github.com/rooreynolds/microprinter
                  http://www.ladyada.net/products/thermalprinter/
                  https://github.com/adafruit/Adafruit-Thermal-Printer-Library/
- * History....:  2012-03-13 V0.1 - first beta
+ * History....:  2010-06-05      – first show of my thermal printer project 
+ *                                 on a public lecture at «Der Tag der Schrift» 
+ *                                 http://www.typo-online.ch/tds/2010/
+ *               2012-03-13 V0.1 - first beta of the class extracted of the project
  *               2012-04-13 V0.5 - first public release
  *
  ********************************************/
