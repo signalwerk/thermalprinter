b6e69ec1d9eeab31cd4177c6c75c45fec0827364
docwisdom <brian@docwisdom.com>
2014-10-20 10:50:05 -0700

    added barcode functions

--- thermalprinter.cpp
+++ thermalprinter.cpp
@@ -139,26 +139,26 @@ void Epson::justifyRight() {
   this->write(2);
 }
 //n range 1-255
-void barcodeHeight(uint8_t n); {
+void Epson::barcodeHeight(uint8_t n); {
   this->write(0x1D);  
   this->write(0x68);    
   this->write(n);
 }
 //n range 2-6
-void barcodeWidth(uint8_t n); {
+void Epson::barcodeWidth(uint8_t n); {
   this->write(0x1D);  
   this->write(0x77);    
   this->write(n);
 }
 //n range 0-3
-void barcodeNumberPosition(uint8_t n); {
+void Epson::barcodeNumberPosition(uint8_t n); {
   this->write(0x1D);  
   this->write(0x48);    
   this->write(n);
 }
 //m range 65-73 (code type)
 //n (digit length)
-void printBarcode(uint8_t m, uint8_t n); {
+void Epson::printBarcode(uint8_t m, uint8_t n); {
   this->write(0x1D);  
   this->write(0x6B);    
   this->write(m);
