4c394bf0885d6966eeb92610bdcb4724983c7ab3
Stefan Huber <sh@signalwerk.ch>
2014-11-25 02:49:56 +0100

    clean up of contributors and readme

---
+++ contributors.md
@@ -0,0 +1,10 @@
+# Contributors (sorted alphabeticaly)
+
+* **[docwisdom](https://github.com/docwisdom)**
+
+  * Added printer status, printing barcodes and justification
+
+* **[Stefan Huber](https://github.com/signalwerk)**
+
+  * Author and maintainer
+  * Most features develpopment
--- thermalprinter.h
+++ thermalprinter.h
@@ -1,27 +1,13 @@
 /*******************************************
  *
  * Name.......:  Epson TM-T88II Library
+ * Version....:  0.6.0
  * Description:  A Library to control an Epson TM-T88II thermal printer (microprinter) 
  *               by an arduino via serial connection
- * Author.....:  Stefan Huber
- * Version....:  0.5
- * Date.......:  2012-04-13
- * Project....:  http://signalwerk.ch
- * Contact....:  sh[at]signalwerk[dot]ch
+ * Project....:  https://github.com/signalwerk/thermalprinter
  * License....:  You may use this work under the terms of either the MIT License or 
                  the GNU General Public License (GPL) Version 3
  * Keywords...:  thermal, micro, receipt, printer, serial, tm-t88, tm88, tmt88, epson
- * Inspiration:  http://tomtaylor.co.uk/projects/microprinter
-                 https://github.com/tomtaylor/microprinter-arduino/tree
-                 http://rooreynolds.com/2009/02/01/microprinter/
-                 https://github.com/rooreynolds/microprinter
-                 http://www.ladyada.net/products/thermalprinter/
-                 https://github.com/adafruit/Adafruit-Thermal-Printer-Library/
- * History....:  2012-04-13 V0.5 - first public release
- *               2012-03-13 V0.1 - first beta of the class extracted of the project
- *               2010-06-05      – first show of my thermal printer project
- *                                 on a public lecture at «Der Tag der Schrift»
- *                                 http://www.typo-online.ch/tds/2010/
  * ********************************************/
 
 #ifndef thermalprinter_h
